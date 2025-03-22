9b734e2579c42131f2b7a50b8b4b98f8ec2ab2a6
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-03-22 14:25:11 +0530

    Add files via upload

---
+++ frontend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the index page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for the loan page
+@app.route('/loan')
+def loan():
+    return render_template('loan.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
